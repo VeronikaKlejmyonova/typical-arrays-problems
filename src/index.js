06dad92b14e2b492ed936576f77dd39b1d29292a
VeronikaKlejmyonova <veronika.klejmyonova@gmail.com>
2021-03-15 22:01:59 +0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+    // метод Array.isArray () проверяет явл ли обЪект массивом
+    // также проверяет, не является ли массив неопределенным или нулевым, если обЪект яв массивом - выдаст значение true 
+    // array.length проверка явл ли массив пустым, если число больше 0 - выдаст значение true
+    // если два условия будут true выполнится условие, иначе возвращает 0
+    // метод Math.min() - ищет минимальное число в массиве
+    // метод Math.max() - ищет максимальное число в массиве
+    // метод array.reduce((a, b) => a + b) - подсчитывается сумму чисел в массиве
+    if (Array.isArray(array) && array.length)
+        return Math.min(...array);
+    return 0;
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length)
+        return Math.max(...array);
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length)
+        return array.reduce((a, b) => a + b) / array.length;
+    return 0;
 }
